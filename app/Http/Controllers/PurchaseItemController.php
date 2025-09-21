14f128c55777454ed4e1d838ff53016319752965
Ozair0101 <ozairjan@gmail.com>
2025-09-21 15:30:40 +0430

    purchase item

---
+++ app/Http/Controllers/PurchaseItemController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PurchaseItem;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PurchaseItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $purchaseItems = PurchaseItem::with(['purchase', 'product'])->get();
+        return response()->json($purchaseItems);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'purchase_id' => 'required|exists:purchases,id',
+            'product_id' => 'required|exists:products,id',
+            'batch_number' => 'nullable|string|max:255',
+            'expiry_date' => 'nullable|date',
+            'quantity' => 'required|numeric|min:0',
+            'unit_cost' => 'required|numeric|min:0',
+            'discount' => 'required|numeric|min:0',
+            'line_total' => 'required|numeric|min:0',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $purchaseItem = PurchaseItem::create($request->all());
+        return response()->json($purchaseItem->load(['purchase', 'product']), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $purchaseItem = PurchaseItem::with(['purchase', 'product'])->findOrFail($id);
+        return response()->json($purchaseItem);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $purchaseItem = PurchaseItem::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'purchase_id' => 'sometimes|required|exists:purchases,id',
+            'product_id' => 'sometimes|required|exists:products,id',
+            'batch_number' => 'nullable|string|max:255',
+            'expiry_date' => 'nullable|date',
+            'quantity' => 'sometimes|required|numeric|min:0',
+            'unit_cost' => 'sometimes|required|numeric|min:0',
+            'discount' => 'sometimes|required|numeric|min:0',
+            'line_total' => 'sometimes|required|numeric|min:0',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $purchaseItem->update($request->all());
+        return response()->json($purchaseItem->load(['purchase', 'product']));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $purchaseItem = PurchaseItem::findOrFail($id);
+        $purchaseItem->delete();
+        return response()->json(null, 204);
+    }
+}
