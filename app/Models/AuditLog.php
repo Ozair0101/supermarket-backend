855ed32cca36a69c23c48637bbd382bd9c25e44d
Ozair0101 <ozairjan@gmail.com>
2025-09-21 15:18:31 +0430

    audil

--- app/Models/AuditLog.php
+++ app/Models/AuditLog.php
@@ -2,9 +2,47 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AuditLog extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'event',
+        'auditable_type',
+        'auditable_id',
+        'old_values',
+        'new_values',
+        'meta',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'old_values' => 'array',
+        'new_values' => 'array',
+        'meta' => 'array',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Get the user for the log.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
