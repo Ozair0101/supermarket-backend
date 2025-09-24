fea22bb618401010a9fca80eb5d43c147e3a75f5
Ozair0101 <ozairjan@gmail.com>
2025-09-24 17:36:42 +0430

    setting

---
+++ app/Http/Controllers/SettingController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Setting;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class SettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $settings = Setting::all();
+        return response()->json($settings);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'key' => 'required|string|unique:settings',
+            'value' => 'required|string',
+            'description' => 'nullable|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $setting = Setting::create($request->all());
+        return response()->json($setting, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $setting = Setting::findOrFail($id);
+        return response()->json($setting);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $setting = Setting::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'key' => 'sometimes|required|string|unique:settings,key,'.$id,
+            'value' => 'sometimes|required|string',
+            'description' => 'nullable|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $setting->update($request->all());
+        return response()->json($setting);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $setting = Setting::findOrFail($id);
+        $setting->delete();
+        return response()->json(null, 204);
+    }
+}
