580fa3a8666804799342d4207539080f57844fc2
Ozair0101 <ozairjan@gmail.com>
2025-09-24 17:38:54 +0430

    customers

---
+++ database/migrations/2025_09_24_130819_add_fields_to_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->string('name');
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->text('address')->nullable();
+            $table->text('notes')->nullable();
+            $table->decimal('remaining_balance', 10, 2)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropColumn([
+                'name',
+                'phone',
+                'email',
+                'address',
+                'notes',
+                'remaining_balance'
+            ]);
+        });
+    }
+};
