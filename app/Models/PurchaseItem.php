513a62a542f31931293b06e5597b147ac8a6cef4
Ozair0101 <ozairjan186@gmail.com>
2025-10-11 03:50:47 -0700

    purcashe

--- app/Models/PurchaseItem.php
+++ app/Models/PurchaseItem.php
@@ -18,10 +18,12 @@ class PurchaseItem extends Model
     protected $fillable = [
         'purchase_id',
         'product_id',
+        'barcode',
         'batch_number',
         'expiry_date',
         'quantity',
         'unit_cost',
+        'selling_price',
         'discount',
         'line_total',
     ];
@@ -34,6 +36,9 @@ class PurchaseItem extends Model
     protected $casts = [
         'quantity' => 'decimal:2',
         'unit_cost' => 'decimal:2',
+        'cost_price' => 'decimal:2',
+        'selling_price' => 'decimal:2',
+        'current_quantity' => 'decimal:2',
         'discount' => 'decimal:2',
         'line_total' => 'decimal:2',
         'expiry_date' => 'date',
